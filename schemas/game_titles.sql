7627e67223d05c4aa649362ccfedfd2e635f0fb5
Pedro Ladaria <pedro.ladaria@gmail.com>
2017-01-15 17:40:25 +0100

    add schemas

--- schemas/game_titles.sql
+++ schemas/game_titles.sql
@@ -1,6 +1,6 @@
-CREATE TABLE game_publishers (
+CREATE TABLE game_titles (
 	game_id UNSIGNED INT(8),
-	company_id UNSIGNED INT(8),
+	genre_id UNSIGNED INT(8),
 	order UNSIGNED TINYINT(3),
 	country TINYINT(3),
 	region TINYINT(1),
