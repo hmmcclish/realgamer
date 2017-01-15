594f12fd422cd7355bf299a24b41004832497069
Pedro Ladaria <pedro.ladaria@gmail.com>
2017-01-15 23:29:50 +0100

    lint rules

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,11 @@
 const Hapi = require('hapi');
+const fs = require('fs');
 
 const server = new Hapi.Server();
+
 server.connection({ port: 3000, host: 'localhost' });
 
 server.start((err) => {
-
     if (err) {
         throw err;
     }
@@ -12,23 +13,33 @@ server.start((err) => {
 });
 
 server.route({
-	method: 'GET',
-	path: '/',
-	handler: (request, reply) => {
-		reply('Hi!\n');
-	},
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        reply('Hi!\n');
+    },
 })
 
 server.route({
-	method: 'GET',
-	path: '/platforms',
-	handler: (request, reply) => {
-		const lines = require('fs').readFileSync('../data/platforms.csv').toString().trim().split('\n');
-		lines.shift(); // skip first row
-		const result = lines.map((line, id) => {
-			const [manufacturer, name, year, generation] = line.split('\t');
-			return {id, manufacturer, name, year, generation};
-		});
-		reply(result);
-	},
+    method: 'GET',
+    path: '/platforms',
+    handler: (request, reply) => {
+        const result = fs.readFileSync('../data/platforms.csv').toString().trim().split('\n').slice(1).map((line, id) => {
+            const [manufacturer, name, year, generation] = line.split('\t');
+            return {id, manufacturer, name, year: +year, generation: +generation};
+        });
+        reply(result);
+    },
+});
+
+server.route({
+    method: 'GET',
+    path: '/games',
+    handler: (request, reply) => {
+        const result = fs.readFileSync('../data/games-sega-saturn.csv').toString().trim().split('\n').slice(1).map((line, id) => {
+            const [title, developer, publisher, yearNa, yearPal, yearJap] = line.split('\t');
+            return {id, title, developer, publisher, year: +yearNa || +yearPal || +yearJap || 0};
+        });
+        reply(result);
+    },
 })
