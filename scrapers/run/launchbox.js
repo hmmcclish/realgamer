d7aaa33794af9d42d12b79c28aa5dfd894020680
Pedro Ladaria <pladaria@tuenti.com>
2017-02-06 21:26:44 +0100

    launchbox dl

--- scrapers/run/launchbox.js
+++ scrapers/run/launchbox.js
@@ -1,7 +1,35 @@
 // @flow
+const launchbox = require('../launchbox');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const path = require('path');
+
+const END = 50000;
+const OUT = path.join(process.env.HOME, '/download/launchbox/');
+
+mkdirp.sync(OUT);
+
+let id = 1;
+
 const util = require('util');
 
 const print = o =>
     console.log(util.inspect(o, {colors: true, depth: 10}));
 
-require('../launchbox').fetchAll(15).then(print);
+const run = () =>
+    launchbox.fetchAll(id)
+        .then(res => {
+            fs.writeFileSync(path.join(OUT, id + '.json'), JSON.stringify(res, null, '  '));
+            return res;
+        })
+        .then(print)
+        .then(() => {
+            id++;
+            if (id < END) run();
+        })
+        .catch(() => {
+            id++;
+            if (id < END) run();
+        });
+
+run();
